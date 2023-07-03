6316d88e56cd49a0d94c5f79c9e5903b851bda5b
Elleined <demadegu@gmail.com>
2023-07-03 15:42:35 +0800

    Added BookMapper with list of genre name

---
+++ src/main/java/com/denielle/api/restapi/mapper/BookMapper.java
@@ -0,0 +1,18 @@
+package com.denielle.api.restapi.mapper;
+
+import com.denielle.api.restapi.dto.BookDTO;
+import com.denielle.api.restapi.model.Book;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = GenreMapper.class)
+public interface BookMapper {
+
+    @Mapping(target = "authorName", source = "author.name")
+    BookDTO toDTO(Book book);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "genres", ignore = true)
+    Book toEntity(BookDTO bookDTO);
+}
--- src/main/java/com/denielle/api/restapi/mapper/GenreMapper.java
+++ src/main/java/com/denielle/api/restapi/mapper/GenreMapper.java
@@ -6,11 +6,21 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+import java.util.Set;
+
 @Mapper(componentModel = "spring")
 public interface GenreMapper {
+
     GenreDTO toDTO(Genre genre);
 
     @Mapping(target = "bookGenres", ignore = true)
     @InheritInverseConfiguration
     Genre toEntity(GenreDTO genreDTO);
+
+    default List<String> mapGenreName(Set<Genre> genres) {
+        return genres.stream()
+                .map(Genre::getName)
+                .toList();
+    }
 }
