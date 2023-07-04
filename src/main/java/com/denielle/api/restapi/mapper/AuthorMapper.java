476356ac010041e043b4500d575265a480dc55f1
Elleined <demadegu@gmail.com>
2023-07-04 14:28:33 +0800

    Added beforeMapping and afterMapping and dependecy injection in mapper class

--- src/main/java/com/denielle/api/restapi/mapper/AuthorMapper.java
+++ src/main/java/com/denielle/api/restapi/mapper/AuthorMapper.java
@@ -2,19 +2,63 @@ package com.denielle.api.restapi.mapper;
 
 import com.denielle.api.restapi.dto.AuthorDTO;
 import com.denielle.api.restapi.model.Author;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.denielle.api.restapi.service.GenreService;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring", uses = BookMapper.class)
-public interface AuthorMapper {
+@Mapper(componentModel = "spring",
+        uses = BookMapper.class)
+public abstract class AuthorMapper implements BaseMapper<AuthorDTO, Author> {
+
+    @Autowired
+    protected GenreService genreService;
 
     @Mapping(target = "books", source = "bookList")
-    AuthorDTO toDTO(Author author);
+    public abstract AuthorDTO toDTO(Author author);
 
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "bookCount", ignore = true)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "bookList", ignore = true)
-    Author toEntity(AuthorDTO authorDTO);
+    @Mappings({
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true),
+            @Mapping(target = "bookCount", ignore = true),
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "bookList", ignore = true)
+    })
+    public abstract Author toEntity(AuthorDTO authorDTO);
+
+    @BeforeMapping
+    protected void beforeMapping(AuthorDTO authorDTO) {
+        System.out.println("\n ===toEntity before mapping===");
+
+        System.out.println("AuthorDTO " + authorDTO);
+
+        System.out.println("===End of toEntity before mapping \n");
+    }
+
+    @AfterMapping
+    protected void afterMapping(AuthorDTO authorDTO) {
+        System.out.println(genreService.getById(1) + " Genre serivec ");
+        System.out.println("\n ===toEntity after mapping===");
+
+        System.out.println("AuthorDTO " + authorDTO);
+
+        System.out.println("===End of toEntity after mapping \n");
+    }
+
+    @BeforeMapping
+    protected void beforeMapping(Author author) {
+        System.out.println("\n ===toDTO before mapping===");
+
+        System.out.println("Author " + author);
+
+        System.out.println("===End of toDTO before mapping \n");
+    }
+
+    @AfterMapping
+    protected void afterMapping(Author author) {
+        System.out.println("\n ===toDTO after mapping===");
+
+        System.out.println("Author " + author);
+
+        System.out.println("===End of toDTO after mapping \n");
+    }
 }
---
+++ src/main/java/com/denielle/api/restapi/mapper/BaseMapper.java
@@ -0,0 +1,11 @@
+package com.denielle.api.restapi.mapper;
+
+import java.util.List;
+
+public interface BaseMapper<DTO, ENTITY> {
+
+    DTO toDTO(ENTITY entity);
+    ENTITY toEntity(DTO dto);
+    List<DTO> toDTOList(List<ENTITY> entities);
+    List<ENTITY> toEntityList(List<DTO> dtos);
+}
