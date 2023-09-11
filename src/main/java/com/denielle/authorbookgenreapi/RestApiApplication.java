5eecaccdcf338ac99b5c463604e3b3aef3e80eba
Elleined <demadegu@gmail.com>
2023-09-11 13:09:12 +0800

    removed eureka client will be added again soon

--- src/main/java/com/denielle/authorbookgenreapi/RestApiApplication.java
+++ src/main/java/com/denielle/authorbookgenreapi/RestApiApplication.java
@@ -2,12 +2,10 @@ package com.denielle.authorbookgenreapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableDiscoveryClient
 public class RestApiApplication {
 
 	public static void main(String[] args) {
