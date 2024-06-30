64abd909848aac1daebd70fae2bca59870f52218
Elleined <demadegu@gmail.com>
2024-06-30 17:51:05 +0800

    Added discovery client

--- src/main/java/com/elleined/libraryapi/LibraryAPIApplication.java
+++ src/main/java/com/elleined/libraryapi/LibraryAPIApplication.java
@@ -2,9 +2,11 @@ package com.elleined.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableTransactionManagement
 public class LibraryAPIApplication {
 
