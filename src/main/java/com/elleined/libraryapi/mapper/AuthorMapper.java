35c02c6178be5031703dcbc756828a88bf736ffa
Elleined <demadegu@gmail.com>
2024-06-30 18:16:10 +0800

    Refactor mapper package

--- src/main/java/com/elleined/libraryapi/mapper/AuthorMapper.java
+++ src/main/java/com/elleined/libraryapi/mapper/AuthorMapper.java
@@ -13,21 +13,24 @@ public interface AuthorMapper extends CustomMapper<Author, AuthorDTO> {
     @Override
     @Mappings({
             @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-            @Mapping(target = "biography", source = "biography"),
             @Mapping(target = "createdAt", source = "createdAt"),
             @Mapping(target = "updatedAt", source = "updatedAt"),
-            @Mapping(target = "bookIds", expression = "java(author.getBookIds())")
+
+            @Mapping(target = "name", source = "name"),
+            @Mapping(target = "biography", source = "biography"),
     })
     AuthorDTO toDTO(Author author);
 
     @Mappings({
             @Mapping(target = "id", ignore = true),
-            @Mapping(target = "name", expression = "java(name)"),
-            @Mapping(target = "biography", expression = "java(biography)"),
             @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
             @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
+
+            @Mapping(target = "name", expression = "java(name)"),
+            @Mapping(target = "biography", expression = "java(biography)"),
+
             @Mapping(target = "books", expression = "java(new java.util.ArrayList<>())")
     })
-    Author toEntity(String name, @Context String biography);
+    Author toEntity(String name,
+                    String biography);
 }
--- src/main/java/com/elleined/libraryapi/mapper/BookMapper.java
+++ src/main/java/com/elleined/libraryapi/mapper/BookMapper.java
@@ -17,29 +17,34 @@ public interface BookMapper extends CustomMapper<Book, BookDTO> {
     @Override
     @Mappings({
             @Mapping(target = "id", source = "id"),
+            @Mapping(target = "createdAt", source = "createdAt"),
+            @Mapping(target = "updatedAt", source = "updatedAt"),
+
             @Mapping(target = "title", source = "title"),
             @Mapping(target = "isbn", source = "isbn"),
             @Mapping(target = "description", source = "description"),
             @Mapping(target = "publishedDate", source = "publishedDate"),
             @Mapping(target = "pages", source = "pages"),
-            @Mapping(target = "createdAt", source = "createdAt"),
-            @Mapping(target = "updatedAt", source = "updatedAt"),
+            @Mapping(target = "views", source = "views"),
+
             @Mapping(target = "authorId", source = "author.id"),
-            @Mapping(target = "genreIds", expression = "java(book.getGenreIds())")
     })
     BookDTO toDTO(Book book);
 
     @Mappings({
             @Mapping(target = "id", ignore = true),
-            @Mapping(target = "views", expression = "java(0)"),
+            @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
+
             @Mapping(target = "title", expression = "java(title)"),
             @Mapping(target = "isbn", expression = "java(isbn)"),
             @Mapping(target = "description", expression = "java(description)"),
             @Mapping(target = "publishedDate", expression = "java(publishedDate)"),
             @Mapping(target = "pages", expression = "java(pages)"),
-            @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())"),
-            @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())"),
+            @Mapping(target = "views", expression = "java(0)"),
+
             @Mapping(target = "author", expression = "java(author)"),
+
             @Mapping(target = "genres", expression = "java(genres)")
     })
     Book toEntity(String title,
